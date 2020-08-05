c42363ed4f4e4c1596aee247a58255e638ef56ef
Michael <michaelbruckbauer@gmx.at>
2020-08-05 12:38:18 +0200

    Update

--- src/JavaExercise02.java
+++ src/JavaExercise02.java
@@ -8,10 +8,10 @@ public class JavaExercise02 {
 
         System.out.println("(a && b) || c");
         System.out.println(" A    | B    |  C    | ? ");
-        for (int i = 1;i < 3;i++) {
-            for (int j = 1;j < 3;j++) {
-                for (int k = 1;k <3;k++) {
-                    System.out.print(a+" | "+b+" | "+c+" | ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    System.out.print(a + " | " + b + " | " + c + " | ");
                     System.out.println((a && b) || c);
                     c = !c;
                 }
@@ -22,10 +22,10 @@ public class JavaExercise02 {
         System.out.println();
         System.out.println("!(a && b) || c");
         System.out.println(" A    | B    |  C    | ? ");
-        for (int i = 1;i < 3;i++) {
-            for (int j = 1;j < 3;j++) {
-                for (int k = 1;k <3;k++) {
-                    System.out.print(a+" | "+b+" | "+c+" | ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    System.out.print(a + " | " + b + " | " + c + " | ");
                     System.out.println(!(a && b) || c);
                     c = !c;
                 }
@@ -36,10 +36,10 @@ public class JavaExercise02 {
         System.out.println();
         System.out.println("!(a || b || c");
         System.out.println(" A    | B    |  C    | ? ");
-        for (int i = 1;i < 3;i++) {
-            for (int j = 1;j < 3;j++) {
-                for (int k = 1;k <3;k++) {
-                    System.out.print(a+" | "+b+" | "+c+" | ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    System.out.print(a + " | " + b + " | " + c + " | ");
                     System.out.println(!(a || b || c));
                     c = !c;
                 }
@@ -50,10 +50,10 @@ public class JavaExercise02 {
         System.out.println();
         System.out.println("!((a && b) || (b && c))");
         System.out.println(" A    | B    |  C    | ? ");
-        for (int i = 1;i < 3;i++) {
-            for (int j = 1;j < 3;j++) {
-                for (int k = 1;k <3;k++) {
-                    System.out.print(a+" | "+b+" | "+c+" | ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    System.out.print(a + " | " + b + " | " + c + " | ");
                     System.out.println(!((a && b) || (b && c)));
                     c = !c;
                 }
@@ -64,10 +64,10 @@ public class JavaExercise02 {
         System.out.println();
         System.out.println("!(a ^ b)");
         System.out.println(" A    | B    | ? ");
-        for (int i = 1;i < 3;i++) {
-            for (int j = 1;j < 3;j++) {
-                    System.out.print(a+" | "+b+" | ");
-                    System.out.println(!(a ^ b));
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(a + " | " + b + " | ");
+                System.out.println(!(a ^ b));
                 b = !b;
             }
             a = !a;
@@ -78,16 +78,17 @@ public class JavaExercise02 {
         boolean p = true;
         boolean q = true;
 
-        System.out.println("!p || (p && q)");
+        System.out.println("!p || q)");
         System.out.println(" p    | q    | ? ");
-        for (int i = 1;i < 3;i++) {
-            for (int j = 1;j < 3;j++) {
-                System.out.print(p+" | "+q+" | ");
-                System.out.println(!p || (p && q));
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(p + " | " + q + " | ");
+                System.out.println(!p || q);
                 q = !q;
             }
             p = !p;
         }
-
     }
 }
+
+
